0c7eca861aed6845d15dc5bb0b9be0d85a27b8cf
richielollis <richielollisdesktop@richies-mini.lan>
2022-08-20 12:05:27 -0400

    add ability to click on repo and be directed to single repo page and account for errors

--- assets/js/single-repo.js
+++ assets/js/single-repo.js
@@ -1,5 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+
+
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -17,7 +31,7 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          document.location.replace("./index.html"); 
         }
     });
 
@@ -74,4 +88,4 @@ var displayWarning = function(repo) {
 };
   
 
-getRepoIssues("expressjs/express");
+getRepoName();
